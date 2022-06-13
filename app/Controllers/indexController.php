399a8ea7bb5e443805b30dd6c5caed91bfe0a43b
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2022-06-13 19:00:00 -0300

    Arquivo de configurações (Bootstrap)

--- app/Controllers/indexController.php
+++ app/Controllers/indexController.php
@@ -4,8 +4,25 @@ namespace app\Controllers;
 
 class indexController
 {
+
+    private $view;
+
+    public function __construct()
+    {
+        $this->view = new \stdClass();
+    }
+
+    public function render($view)
+    {
+        $class = get_class($this);
+        $class = str_replace('app\\Controllers\\', '', $class);
+        $class = strtolower(str_replace('Controller', '', $class));
+        
+        require_once "../app/Views/".$class."/".$view.".phtml";
+    }
+
     public function index()
     {
-        echo "Hello World PHP!";
+        $this->render('index');
     }
 }
---
+++ vendor/dmf/init/Bootstrap.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace dmf\init;
+
+abstract class Bootstrap
+{
+    private $routes;
+
+    abstract protected function initRoutes();
+
+    public function __construct()
+    {
+        $this->initRoutes();
+        $this->run($this->getUrl());
+    }
+
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
+
+    public function setRoutes(array $routes)
+    {
+        $this->routes = $routes;
+    }
+
+    protected function getUrl()
+    {
+        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    }
+
+    protected function run($url)
+    {
+        foreach($this->getRoutes() as $key => $route)
+        {
+            if($url == $route['route'])
+            {
+                $class = "app\\Controllers\\" . ucfirst($route['controller']);
+                $controller = new $class;
+                
+                $action = $route['action'];
+                $controller->$action();
+            }
+        }
+    }
+}
