d22b89b0f4fa5b3695bd24d2c8201516548e0a62
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2022-06-15 15:41:56 -0300

    Definição e reutilização de layout base

--- vendor/dmf/controller/Action.php
+++ vendor/dmf/controller/Action.php
@@ -13,10 +13,16 @@ abstract class Action
 
     protected function render($view)
     {
+        $this->view->page = $view;
+        require_once "../app/Views/layoutExample.phtml";
+    }
+
+    protected function content()
+    {
         $class = get_class($this);
         $class = str_replace('app\\Controllers\\', '', $class);
         $class = strtolower(str_replace('Controller', '', $class));
         
-        require_once "../app/Views/".$class."/".$view.".phtml";
+        require_once "../app/Views/".$class."/".$this->view->page.".phtml";
     }
 }
