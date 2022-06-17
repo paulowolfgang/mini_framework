0247abdb17fe48f9a68c8780d18e42efcc91358e
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2022-06-16 23:54:11 -0300

    Classe de conexão com o banco de dados

---
+++ app/Connection.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app;
+
+class Connection
+{   
+    private static $host = "localhost";
+    private static $type = "mysql";
+    private static $port = "3306";
+    private static $user = "root";
+    private static $name = "mini_framework";
+    private static $pass = "123";
+
+    private function getHost()
+    {
+        return self::$host;
+    }
+
+    private function getType()
+    {
+        return self::$type;
+    }
+
+    private function getPort()
+    {
+        return self::$port;
+    }
+
+    private function getUser()
+    {
+        return self::$user;
+    }
+
+    private function getName()
+    {
+        return self::$name;
+    }
+
+    private function getPass()
+    {
+        return self::$pass;
+    }
+
+    public function connect()
+    {   
+        try {
+            $this->connection = new PDO($this->getType().":host=".$this->getHost().";port=".$this->getPort().";dbname=".$this->getName(),
+                                    $this->getUser(),
+                                    $this->getPass());
+        } catch (PDOException $e){
+            //..
+        }
+        return ($this->connection);
+    }
+}
